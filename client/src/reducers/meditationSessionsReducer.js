fa8c830936297b1b2b32af06e7c26f0ad756aecc
Jordan Kline <jordan.kline@gmail.com>
2018-07-13 09:39:01 -0700

    added state update to individual meditation session update

--- client/src/reducers/meditationSessionsReducer.js
+++ client/src/reducers/meditationSessionsReducer.js
@@ -6,25 +6,24 @@ const setMeditationSessions = (meditationSessions) => {
 	return { type: "GET_USER_MEDITATION_SESSIONS", meditationSessions }
 }
 
-export const removeMeditationSessions = () => {
-	return { type: "RESET_USER_MEDITATION_SESSIONS" }
-}
-
 export const getUserMeditationSessions = (currentUser) => dispatch => {
 	fetch(`http://localhost:3001/users/${currentUser.id}/meditation_sessions.json`) // , {mode: 'cors', creditials: 'include' })
 		.then(res => { return res.json() })
 		.then(json => { dispatch(setMeditationSessions(json))})
 }
 
-// const deleteMeditationSession = (meditationSession) => {
-// 	return { type: "DELETE_"}
-// }
+const removeMeditationSession = (session) => {
+	return { type: "REMOVE_MEDITATION_SESSION", session}
+}
 
 export const deleteMeditationSession = (currentUser, session) => dispatch => {
 	fetch(`http://localhost:3001/users/${currentUser.id}/meditation_sessions/${session}`, {
 		method: "DELETE"
-	})
+		})
+		.then(() => { dispatch(removeMeditationSession(session))})
 }
+
+
 
 export default function meditationSessionsReducer(state = initialState, action) {
 	switch (action.type) {
@@ -37,6 +36,11 @@ export default function meditationSessionsReducer(state = initialState, action) 
 			return {
 				...state,
 				meditationSessions: [],
+			}
+		case "REMOVE_MEDITATION_SESSION":
+			return {
+				...state,
+				meditationSessions: state.meditationSessions.filter(sess => { return sess.id !== action.session }),
 			}
 		default:
 			return state;
